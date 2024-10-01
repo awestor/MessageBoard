162657cdc9446fc39a233c1e4ba31d4981739735
awester <avilovd200@gmail.com>
2024-10-01 23:04:54 +0300

    Repair migration and add connection fragment

---
+++ OrderBoard.AppServices/Adverts/Services/AdvertService.cs
@@ -0,0 +1,19 @@
+﻿using OrderBoard.AppServices.Adverts.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderBoard.AppServices.Adverts.Services
+{
+    public class AdvertService : IAdvertService
+    {
+        private readonly IAdvertRepository _advertRepository;
+
+        public AdvertService(IAdvertRepository advertRepository)
+        {
+            _advertRepository = advertRepository;
+        }
+    }
+}
---
+++ OrderBoard.DataAccess/Repositories/AdvertRepository.cs
@@ -0,0 +1,13 @@
+﻿using OrderBoard.AppServices.Adverts.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderBoard.DataAccess.Repositories
+{
+    public class AdvertRepository : IAdvertRepository
+    {
+    }
+}
